509418641d07c348e5ad1ae6930a5810139783b0
Sunbin-Lee <81344661+Sunbin-Lee@users.noreply.github.com>
2024-02-19 20:03:30 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,32 @@
+def compression(s, l):
+    now = s[:l]
+    count = 1
+    idx = l
+    compressed = ''
+    while idx + l <= len(s):
+        next = s[idx:idx+l]
+        if next == now:
+            count += 1
+        else:
+            if count > 1:
+                compressed += str(count)
+            compressed += now
+            now = next
+            count = 1
+        idx += l
+    
+    if count > 1:
+        compressed += str(count)
+    compressed += next
+    
+    if idx < len(s):
+        compressed += s[idx:]
+    
+    return compressed
+
+def solution(s):
+    answer = len(s)
+    for i in range(1, len(s)//2+1):
+        answer = min(answer, len(compression(s, i)))
+    
+    return answer
