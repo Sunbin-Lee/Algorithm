05c4bd6b82ed2c30ab9598de02e79242743b4b3b
Sunbin-Lee <81344661+Sunbin-Lee@users.noreply.github.com>
2024-02-26 19:27:34 +0900

    [Gold IV] Title: 뱀, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,55 +1,44 @@
 from collections import deque
 
 N = int(input())
+map_arr = [[False for _ in range(N)] for _ in range(N)]
 K = int(input())
-
-arr = [[0 for _ in range(N)] for _ in range(N)]
-
 for _ in range(K):
-    apple_x, apple_y = map(int, input().split())
-    arr[apple_x-1][apple_y-1] = 2
+    a, b = map(int, input().split())
+    map_arr[a-1][b-1] = True
 
+commands = deque([])
 L = int(input())
-commands = []
 for _ in range(L):
-    t, d = input().split()
-    if d == 'L':
-        d = -1
-    else:
-        d = 1
-    commands.append((int(t), d))
+    X, C = input().split()
+    commands.append((int(X), C))
 
-steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-start_x, start_y = 0, 0
-q = deque([(start_x, start_y)])
-arr[start_x][start_y] = 1
-
-start_d = 0 # 방향
-time = 0
-
+snakes = deque([(0, 0)])
+steps = [(0,1), (1,0), (0,-1), (-1,0)]
+d = 0
+T = 0
 while True:
-    time += 1
+    if commands and T == commands[0][0]:
+        X, C = commands.popleft()
+        if C == 'D':
+            d = (d+1)%4
+        elif C == 'L':
+            d = (d+3)%4
 
-    new_x = start_x + steps[start_d][0]
-    new_y = start_y + steps[start_d][1]
+    T += 1
+    head_i, head_j = snakes[-1]
 
-    if new_x < 0 or new_x >= N or new_y < 0 or new_y >= N:
+    next_i = head_i + steps[d][0]
+    next_j = head_j + steps[d][1]
+    if next_i < 0 or next_i >= N or next_j < 0 or next_j >= N: # 벽
         break
-    if arr[new_x][new_y] == 1:
+    if (next_i, next_j) in snakes: # 몸통
         break
+    snakes.append((next_i, next_j))
 
-    q.append((new_x, new_y))
-    if arr[new_x][new_y] == 0:
-        x, y = q.popleft()
-        arr[x][y] = 0
+    if map_arr[next_i][next_j]: # 사과
+        map_arr[next_i][next_j] = False
+    else:
+        snakes.popleft()
 
-    arr[new_x][new_y] = 1
-    start_x, start_y = new_x, new_y
-
-    if commands and time == commands[0][0]: # 다음 턴의 방향 정하기
-        start_d += commands[0][1]
-        start_d = start_d % 4
-        commands.pop(0)
-
-print(time)
+print(T)
